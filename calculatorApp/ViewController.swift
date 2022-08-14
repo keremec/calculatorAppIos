68910e3f11a417964809ec67ba2fc2405eaeeee4
keremec <k.safa.dirican@outlook.com>
2022-08-15 01:21:11 +0300

    bug fixes

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -32,14 +32,23 @@ class ViewController: UIViewController {
     @IBAction func buttonDel(_ sender: Any) {
         _ = bLabel.text?.popLast()
         var exp = bLabel.text!
+        
+        //Auto add 0 to main label and clear secondary label if main label empty
         if(bLabel.text?.count == 0){
             bLabel.text = "0"
             tLabel.text = ""
         }
-        else if (exp.contains("e")){
+        //delete button will act like clear button if a large number, x/0 result or somehow irrational etc received
+        else if (exp.contains("e") || exp.contains("i") ){
             tLabel.text = ""
             bLabel.text = "0"
         }
+        // prevent calculation when deleting if - remain as a result of a negative calculation
+        else if(bLabel.text == "-"){
+            tLabel.text = ""
+            return
+        }
+        // do calculation when deleting characters also if exp.last is a operator, this deletes tge last operator from calculation string
         else if(exp.contains("+")||exp.contains("-")||exp.contains("×")||exp.contains("÷")||exp.contains("%")){
             if(!listOfNumbers.contains(exp.last!)){
                 _ = exp.popLast()
@@ -132,15 +141,16 @@ class ViewController: UIViewController {
 
 func buttonClick(b:String, s:inout String, t:inout String){
 
+    // auto clear results if a large number, x/0 result or somehow irrational etc received
+    if (s.contains("e") || s.contains("i") ){
+        t = ""
+        s = "0"
+    }
     
     switch b {
         
     case ".":
-        //prevent adding more zeros to the start
-        if(s.count == 0 ){
-            s += "0."
-        }
-        else if(s.last == "."){
+        if(s.last == "."){
             break
         }
         else if(!listOfNumbers.contains(s.last!)){
@@ -151,13 +161,28 @@ func buttonClick(b:String, s:inout String, t:inout String){
         }
         
     case "+", "-", "÷", "×", "%":
-        if(s.count == 0 ){
-           break
+        // allows using minus operator to use when writing negative numbers
+        // user cant add + to the start because default is already positive
+        if(s == "0" ){
+            if(b == "-"){
+                _ = s.popLast()
+                s += b
+            }
+            break
         }
+        //because minus button not acting as operator but as a negative symbol
+        //this one simply change - to + (or revese) instead of just removing - for better feedback
+        //+ will be removed when entering number
+        else if((s == "-" || s == "+") && (b == "-" || b == "+")){
+            _ = s.popLast()
+            s += b
+        }
+        // turns this "2.+3" to this "2+3"
         else if(s.last == "."){
             _ = s.popLast()
             s += b
         }
+        // prevents "+*" situation
         else if(!listOfNumbers.contains(s.last!)){
             break
         }
@@ -174,15 +199,13 @@ func buttonClick(b:String, s:inout String, t:inout String){
         }
         
     default:
-        //remove unwanted zero
-        if(s.count == 1 && s.last == "0"){
+        //removes placeholder zero
+        if(s == "0" || s == "+"){
             _ = s.popLast()
         }
         s += b
     }
     
-    
-    //Preperaing for NSExpression
     
 
     
@@ -194,30 +217,42 @@ func buttonClick(b:String, s:inout String, t:inout String){
 
 }
 
+
+// calculation function
 func calculateResult(s:String) -> Double {
     
     
     var exp = s
     
+    // change visual operators to real operators for nsexpression
     exp = exp.replacingOccurrences(of: "×", with: "*")
     exp = exp.replacingOccurrences(of: "÷", with: "/")
     exp = exp.replacingOccurrences(of: "%", with: "/100*")
     
+    //to get a double result from NSEexpression, one of the input should be double. This function do that.
     exp = fixDecimalDivision(s: exp)
     
     let expression = NSExpression(format: exp)
     
-    if let result = expression.expressionValue(with: nil, context: nil) as? Double{
+    if var result = expression.expressionValue(with: nil, context: nil) as? Double{
+        //round the decimal points
+        result = Double(round(1000000*result)/1000000)
         return result
     }
     else{
-        return -1
+        //      LEBLEBI
+        return -1837837
     }
     
 }
 
 func fixDecimalDivision(s:String) -> String {
     var exp = s
+    //because only division operator can create a non Integer result
+    //this function split the string by other operators and find the division operations
+    //if division operations don't have decimal point they are adding to the toDel array
+    //because having only one double operand is enough for gettin double result
+    // this funcion adds .0 to the second operand of the divison operation
     let splits = exp.components(separatedBy: ["+", "*", "-"])
     var toDel: [String] = []
     for i in splits{
@@ -234,6 +269,7 @@ func fixDecimalDivision(s:String) -> String {
 }
 
 
+// cool extension for double. If decimal point is zero, it just removes it. also return it as a string
 extension Double {
     var cleanValue: String {
        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
